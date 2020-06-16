b832cc005fe72dec1c81c48b06b0162dc3611779
Hegar Garcia <hegargarcia@gmail.com>
2020-06-16 12:02:12 -0500

    Fixing integration with firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,40 @@
-import { Fab, ListItemSecondaryAction } from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import { Add, PlayArrow, Reorder } from '@material-ui/icons';
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import moment from 'moment';
 import AddExercise from '../components/AddExercise';
 import DaySelector from '../components/DaySelector';
 import { LayoutContext } from '../context/layout';
 
 const formatter = new Intl.NumberFormat();
-
 const getDateFormated = () => new Date().toDateString().substr(3, 7);
+
+const initialState = {
+  modalStatus: false,
+  selectedDay: moment().startOf('day').format('YYYY MM DD'),
+  exercises: []
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'open':
+      return { ...state, modalStatus: true };
+    case 'close':
+      return { ...state, modalStatus: false };
+    case 'selectDay':
+      return { ...state, selectedDay: action.payload };
+    case 'addExercise':
+      return { ...state, exercises: [...state.exercises, action.payload] };
+    default:
+      return state;
+  }
+};
 
 const Exercise = ({ exercise, reps }) => (
   <ListItem>
@@ -25,6 +47,10 @@ const Exercise = ({ exercise, reps }) => (
     </ListItemSecondaryAction>
   </ListItem>
 );
+
+const formatDate = (day) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  moment().date(day).startOf('day').format('YYYY MM DD');
 
 const StyledSection = styled.section`
   width: 100%;
@@ -44,26 +70,32 @@ const FabContainer = styled.div`
 
 const Home = () => {
   const { setMain } = useContext(LayoutContext);
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClose = useCallback(() => setIsOpen(false), []);
-  const handleOpen = useCallback(() => setIsOpen(true), []);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleClose = useCallback(() => dispatch({ type: 'close' }), []);
+  const handleOpen = useCallback(() => dispatch({ type: 'open' }), []);
+  const handleDayChange = useCallback(
+    (day) => dispatch({ type: 'selectDay', payload: formatDate(day) }),
+    []
+  );
+  const addExercise = useCallback(
+    (exercise) => {
+      dispatch({ type: 'addExercise', payload: exercise });
+      handleClose();
+    },
+    [handleClose]
+  );
 
   useEffect(() => {
     setMain({ title: getDateFormated() });
   }, [setMain]);
 
-  const exercises = [
-    { name: 'Pullups', reps: 69420 },
-    { name: 'Squads', reps: 30 },
-    { name: 'Pushups', reps: 40 }
-  ];
-
   return (
     <StyledSection>
-      <DaySelector />
+      <DaySelector onChange={handleDayChange} />
       <List>
-        {exercises.map(({ name, reps }) => (
-          <Exercise key={name + reps} exercise={name} reps={reps} />
+        {state.exercises.map(({ exercise, reps }) => (
+          <Exercise key={exercise + reps} exercise={exercise} reps={reps} />
         ))}
       </List>
       <FabContainer>
@@ -74,7 +106,11 @@ const Home = () => {
           <Add />
         </Fab>
       </FabContainer>
-      <AddExercise isOpen={isOpen} handleClose={handleClose} />
+      <AddExercise
+        isOpen={state.modalStatus}
+        handleClose={handleClose}
+        handleSubmit={addExercise}
+      />
     </StyledSection>
   );
 };
